b3984dd0d01377baf6cab93b7d63da169acfdc97
ThinhBui94 <thinhducbui94@gmail.com>
2017-12-26 16:06:00 +0700

    Login Logout user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/signup", to: "users#new"
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
