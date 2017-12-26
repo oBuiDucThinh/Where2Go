4a081aa6d7da6723d0d2ec9c230a68e849f1861e
ThinhBui94 <thinhducbui94@gmail.com>
2017-12-26 16:06:00 +0700

    Edit user profile page and event bug

---
+++ app/controllers/join_events_controller.rb
@@ -0,0 +1,32 @@
+class JoinEventsController < ApplicationController
+  before_action :logged_in?, only: [:create]
+  def create
+    @user = current_user.id
+    @event = params[:event_id]
+    @current_event = Event.find_by id: @event
+    join = {user_id: @user, event_id: @event}
+    @new_join = JoinEvent.new join
+
+    if @new_join.save
+      @temp = true
+      return @temp
+    else
+      @current_join = JoinEvent.find_by(user_id: @user, event_id: @event)
+      @current_join.destroy
+      @temp = false
+      return @temp
+    end
+
+    respond_to do |format|
+      format.html {redirect_to events_path}
+      format.js
+    end
+  end
+
+  private
+
+  def logged_in?
+    return if current_user.present?
+    redirect_to new_user_path
+  end
+end
