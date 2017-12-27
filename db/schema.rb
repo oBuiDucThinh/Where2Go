9e7b396b897be65bc4f37c9f67f841c128888ce2
hoanx208 <nguyenxuanhoa208@gmail.com>
2017-12-27 10:56:04 +0700

    finish init-models-hnx

--- db/schema.rb
+++ db/schema.rb
@@ -10,60 +10,77 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227015716) do
+ActiveRecord::Schema.define(version: 20171227035104) do
+
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "event_categories", force: :cascade do |t|
-    t.integer "event_id"
-    t.integer "category_id"
+  create_table "cities", force: :cascade do |t|
+    t.string   "city_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "date_created"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "event_categories", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_event_categories_on_category_id"
     t.index ["event_id", "category_id"], name: "index_event_categories_on_event_id_and_category_id", unique: true
     t.index ["event_id"], name: "index_event_categories_on_event_id"
   end
 
-  create_table "user_categories", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "category_id"
+  create_table "event_cities", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_user_categories_on_category_id"
-    t.index ["user_id", "category_id"], name: "index_user_categories_on_user_id_and_category_id", unique: true
-    t.index ["user_id"], name: "index_user_categories_on_user_id"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "content"
-    t.datetime "date_created"
-    t.integer "user_id"
-    t.integer "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_comments_on_event_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["city_id"], name: "index_event_cities_on_city_id"
+    t.index ["event_id"], name: "index_event_cities_on_event_id"
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string   "title"
+    t.text     "content"
     t.datetime "date_start"
     t.datetime "date_end"
-    t.boolean "is_open"
-    t.integer "user_id"
+    t.boolean  "is_open"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "user_categories", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_user_categories_on_category_id"
+    t.index ["user_id", "category_id"], name: "index_user_categories_on_user_id_and_category_id", unique: true
+    t.index ["user_id"], name: "index_user_categories_on_user_id"
+  end
+
   create_table "user_events", force: :cascade do |t|
-    t.boolean "join"
-    t.boolean "like"
-    t.integer "user_id"
-    t.integer "event_id"
+    t.boolean  "join"
+    t.boolean  "like"
+    t.integer  "user_id"
+    t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_user_events_on_event_id"
@@ -71,13 +88,14 @@ ActiveRecord::Schema.define(version: 20171227015716) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "phone"
-    t.integer "role", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "role",            default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
