faf168e2e99a4fc627486a9a96031f3646d4c798
AnnHoang191 <hquynhanh19@gmail.com>
2017-12-27 08:59:19 +0700

    Modeling user-comment-user&event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226144242) do
+ActiveRecord::Schema.define(version: 20171227015716) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,50 @@ ActiveRecord::Schema.define(version: 20171226144242) do
     t.index ["category_id"], name: "index_user_categories_on_category_id"
     t.index ["user_id", "category_id"], name: "index_user_categories_on_user_id_and_category_id", unique: true
     t.index ["user_id"], name: "index_user_categories_on_user_id"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "date_created"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "date_start"
+    t.datetime "date_end"
+    t.boolean  "is_open"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "user_events", force: :cascade do |t|
+    t.boolean  "join"
+    t.boolean  "like"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "role",            default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
