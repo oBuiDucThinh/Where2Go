19e8ca3c5b15aab04b3a474b2fc15b1e7dbbe962
AnnHoang191 <hquynhanh19@gmail.com>
2018-01-04 09:09:03 +0700

    Fix signup bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :user_categories
   accepts_nested_attributes_for :categories
 
-  enum role: {normal: 0, creator: 1}
+  enum role: [:normal, :creator]
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: Settings.user.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.user.email.maximum},
