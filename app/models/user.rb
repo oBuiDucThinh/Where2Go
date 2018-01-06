18846b5e34c845dd995a806b50e71866a2348c30
Thinh Bui <thinhducbui94@gmail.com>
2018-01-06 14:55:56 +0700

    Add and delete comments using ajax

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,24 +11,24 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :categories
 
   enum role: [:normal, :creator, :admin]
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: Settings.user.name.maximum}
-  validates :email, presence: true, 
+  validates :email, presence: true,
     length: {maximum: Settings.user.email.maximum},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
-  validates :phone, presence: true, 
+  validates :phone, presence: true,
     length: {maximum: Settings.user.phone.maximum}
-  validates :password, presence: true, 
+  validates :password, presence: true,
     length: {minimum: Settings.user.password.minimum},
     unless: :skip_password_validation
   has_secure_password
-  
+
   scope :load_info_to_edit, ->{select :name, :email, :phone}
   scope :load_to_show_ordered_by_name,
     ->{select :id, :name, :email, :role, :created_at and order name: :asc}
-  
+
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
