33357ae259897f39b537b991588d9d6e3bda59d7
Thinh Bui <thinhducbui94@gmail.com>
2018-01-16 08:44:08 +0700

    Rails geocoding 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115080544) do
+ActiveRecord::Schema.define(version: 20180116013750) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 20180115080544) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "picture"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
